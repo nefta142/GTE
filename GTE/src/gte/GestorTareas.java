eae02068584cd5cd3b5e4a9effb6efd6a8e718c8
neftali <neftaliespinorodriguez@gmail.com>
2026-03-04 16:40:48 +0000

    Agegar Tareas

--- GTE/src/gte/GestorTareas.java
+++ GTE/src/gte/GestorTareas.java
@@ -2,7 +2,6 @@ package gte;
 
 import java.util.ArrayList;
 
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -13,10 +12,14 @@ import java.util.ArrayList;
  * @author nefta
  */
 public class GestorTareas {
-      private final ArrayList<Tarea> tareas;
+    private final ArrayList<Tarea> tareas;
 
     public GestorTareas() {
         this.tareas = new ArrayList<>();
+    }
+
+    public void añadirTarea(String descripcion) {
+        tareas.add(new Tarea(descripcion));
     }
 
   
--- GTE/src/gte/Main.java
+++ GTE/src/gte/Main.java
@@ -4,6 +4,8 @@
  */
 package gte;
 
+import java.util.Scanner;
+
 /**
  *
  * @author nefta
@@ -14,7 +16,24 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+         Scanner sc = new Scanner(System.in);
+        GestorTareas gestor = new GestorTareas();
+
+        System.out.println("1. Añadir tarea");
+        System.out.print("Seleccione una opción: ");
+
+        int opcion = sc.nextInt();
+        sc.nextLine(); 
+
+        if (opcion == 1) {
+            System.out.print("Introduce la descripción de la tarea: ");
+            String descripcion = sc.nextLine();
+
+            gestor.añadirTarea(descripcion);
+
+            System.out.println("Tarea añadida correctamente.");
+        }
+        
     }
     
 }
